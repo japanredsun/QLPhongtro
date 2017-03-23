f8174d43a7f8ab1b94c19aac554684faf2322af9
KDCuong <toilacaoboivientay96@gmail.com>
2017-03-24 05:18:13 +0700

    Form Information

---
+++ MotelRoomManagement/MotelRoomManagement/frmInformation.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace MotelRoomManagement
+{
+    partial class frmInformation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label12 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lbID = new System.Windows.Forms.Label();
+            this.lbName = new System.Windows.Forms.Label();
+            this.lbNS = new System.Windows.Forms.Label();
+            this.lbCMND = new System.Windows.Forms.Label();
+            this.lbQue = new System.Windows.Forms.Label();
+            this.lbJob = new System.Windows.Forms.Label();
+            this.ptbAnh34 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btUpdate = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbAnh34)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label12
+            // 
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.Blue;
+            this.label12.Location = new System.Drawing.Point(164, 21);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(354, 49);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "Thông tin khách hàng";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã khách hàng:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(11, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 35);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ngày sinh:";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 35);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Số CMND:";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(15, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 35);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Quê quán:";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(17, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(143, 35);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Nghề nghiệp:";
+            // 
+            // label13
+            // 
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(17, 261);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(143, 35);
+            this.label13.TabIndex = 4;
+            this.label13.Text = "Ghi chú:";
+            // 
+            // lbID
+            // 
+            this.lbID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbID.Location = new System.Drawing.Point(180, 22);
+            this.lbID.Name = "lbID";
+            this.lbID.Size = new System.Drawing.Size(218, 23);
+            this.lbID.TabIndex = 5;
+            this.lbID.Text = "DH14TH02";
+            // 
+            // lbName
+            // 
+            this.lbName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbName.Location = new System.Drawing.Point(347, 186);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(234, 35);
+            this.lbName.TabIndex = 5;
+            this.lbName.Text = "Chó kute";
+            this.lbName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbNS
+            // 
+            this.lbNS.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNS.Location = new System.Drawing.Point(180, 66);
+            this.lbNS.Name = "lbNS";
+            this.lbNS.Size = new System.Drawing.Size(218, 23);
+            this.lbNS.TabIndex = 5;
+            this.lbNS.Text = "11/03/2017";
+            // 
+            // lbCMND
+            // 
+            this.lbCMND.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCMND.Location = new System.Drawing.Point(180, 116);
+            this.lbCMND.Name = "lbCMND";
+            this.lbCMND.Size = new System.Drawing.Size(218, 23);
+            this.lbCMND.TabIndex = 5;
+            this.lbCMND.Text = "123456789";
+            // 
+            // lbQue
+            // 
+            this.lbQue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbQue.Location = new System.Drawing.Point(180, 163);
+            this.lbQue.Name = "lbQue";
+            this.lbQue.Size = new System.Drawing.Size(218, 23);
+            this.lbQue.TabIndex = 5;
+            this.lbQue.Text = "TP.HCM";
+            // 
+            // lbJob
+            // 
+            this.lbJob.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbJob.Location = new System.Drawing.Point(180, 213);
+            this.lbJob.Name = "lbJob";
+            this.lbJob.Size = new System.Drawing.Size(218, 23);
+            this.lbJob.TabIndex = 5;
+            this.lbJob.Text = "Hốt rác";
+            // 
+            // ptbAnh34
+            // 
+            this.ptbAnh34.Image = global::MotelRoomManagement.Properties.Resources._12746302_169850233394913_446563990_n;
+            this.ptbAnh34.Location = new System.Drawing.Point(347, 22);
+            this.ptbAnh34.Name = "ptbAnh34";
+            this.ptbAnh34.Size = new System.Drawing.Size(234, 164);
+            this.ptbAnh34.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbAnh34.TabIndex = 6;
+            this.ptbAnh34.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btUpdate);
+            this.panel1.Controls.Add(this.ptbAnh34);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.lbJob);
+            this.panel1.Controls.Add(this.lbQue);
+            this.panel1.Controls.Add(this.lbCMND);
+            this.panel1.Controls.Add(this.lbNS);
+            this.panel1.Controls.Add(this.lbName);
+            this.panel1.Controls.Add(this.lbID);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(39, 84);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(605, 352);
+            this.panel1.TabIndex = 8;
+            // 
+            // btUpdate
+            // 
+            this.btUpdate.AutoSize = true;
+            this.btUpdate.BackColor = System.Drawing.Color.Red;
+            this.btUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btUpdate.ForeColor = System.Drawing.Color.White;
+            this.btUpdate.Location = new System.Drawing.Point(518, 311);
+            this.btUpdate.Name = "btUpdate";
+            this.btUpdate.Size = new System.Drawing.Size(86, 40);
+            this.btUpdate.TabIndex = 7;
+            this.btUpdate.Text = "Update";
+            this.btUpdate.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(180, 261);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(424, 47);
+            this.label7.TabIndex = 5;
+            // 
+            // frmInformation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 490);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label12);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmInformation";
+            this.Text = "Khách Hàng";
+            ((System.ComponentModel.ISupportInitialize)(this.ptbAnh34)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lbID;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label lbNS;
+        private System.Windows.Forms.Label lbCMND;
+        private System.Windows.Forms.Label lbQue;
+        private System.Windows.Forms.Label lbJob;
+        private System.Windows.Forms.PictureBox ptbAnh34;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btUpdate;
+        private System.Windows.Forms.Label label7;
+    }
+}
