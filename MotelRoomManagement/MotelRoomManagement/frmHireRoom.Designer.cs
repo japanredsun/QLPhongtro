532f74b5af22bdf6cc62e1e915dcadbd7123f51e
KDCuong <toilacaoboivientay96@gmail.com>
2017-03-24 05:29:40 +0700

    Form Hire Room

---
+++ MotelRoomManagement/MotelRoomManagement/frmHireRoom.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace MotelRoomManagement
+{
+    partial class frmHireRoom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTNT = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTDC = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtGC = new System.Windows.Forms.TextBox();
+            this.btSave = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên người thuê:";
+            // 
+            // txtTNT
+            // 
+            this.txtTNT.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTNT.Location = new System.Drawing.Point(18, 80);
+            this.txtTNT.Name = "txtTNT";
+            this.txtTNT.Size = new System.Drawing.Size(543, 39);
+            this.txtTNT.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 32);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ngày thuê:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(293, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 32);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tiền đặt cọc:";
+            // 
+            // txtTDC
+            // 
+            this.txtTDC.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTDC.Location = new System.Drawing.Point(299, 199);
+            this.txtTDC.Name = "txtTDC";
+            this.txtTDC.Size = new System.Drawing.Size(250, 39);
+            this.txtTDC.TabIndex = 1;
+            this.txtTDC.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 32);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Ghi chú:";
+            // 
+            // txtGC
+            // 
+            this.txtGC.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGC.Location = new System.Drawing.Point(18, 318);
+            this.txtGC.Multiline = true;
+            this.txtGC.Name = "txtGC";
+            this.txtGC.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtGC.Size = new System.Drawing.Size(543, 104);
+            this.txtGC.TabIndex = 1;
+            // 
+            // btSave
+            // 
+            this.btSave.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSave.ForeColor = System.Drawing.Color.White;
+            this.btSave.Location = new System.Drawing.Point(393, 428);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(73, 42);
+            this.btSave.TabIndex = 2;
+            this.btSave.Text = "Lưu";
+            this.btSave.UseVisualStyleBackColor = false;
+            // 
+            // btCancel
+            // 
+            this.btCancel.BackColor = System.Drawing.Color.Red;
+            this.btCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCancel.ForeColor = System.Drawing.Color.White;
+            this.btCancel.Location = new System.Drawing.Point(473, 428);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(75, 41);
+            this.btCancel.TabIndex = 3;
+            this.btCancel.Text = "Hủy";
+            this.btCancel.UseVisualStyleBackColor = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dateTimePicker1.Location = new System.Drawing.Point(18, 199);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(224, 38);
+            this.dateTimePicker1.TabIndex = 4;
+            this.dateTimePicker1.Value = new System.DateTime(2017, 3, 15, 0, 0, 0, 0);
+            // 
+            // frmHireRoom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 482);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.txtTDC);
+            this.Controls.Add(this.txtGC);
+            this.Controls.Add(this.txtTNT);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmHireRoom";
+            this.Text = "Thuê phòng mới";
+            this.Load += new System.EventHandler(this.frmHireRoom_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTNT;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTDC;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtGC;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
