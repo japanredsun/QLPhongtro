e62ec1c69d833d73b82d9155c41fe12b3ccb0955
KDCuong <toilacaoboivientay96@gmail.com>
2017-04-13 19:42:28 +0700

    UP

---
+++ MotelRoomManagement/MRDTO/GhiNho.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MRDTO
+{
+    public class GhiNho
+    {
+        public string ID { get; set; }
+        public string pass { get; set; }
+        public int aa { get; set; }
+        public GhiNho(string ID, string pass, int aa)
+        {
+            this.ID = ID;
+            this.pass = pass;
+            this.aa = aa;
+        }
+    }
+}
--- MotelRoomManagement/MotelRoomManagement/frmLogin.cs
+++ MotelRoomManagement/MotelRoomManagement/frmLogin.cs
@@ -6,6 +6,10 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
+using MRBUS;
+using MRDTO;
 
 namespace MotelRoomManagement
 {
@@ -14,6 +18,51 @@ namespace MotelRoomManagement
         public frmLogin()
         {
             InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            //string a, b;
+            string sql = "SELECT * FROM GhiNho";
+            List<GhiNho> tmp = new PhongBUS().getGN(sql);
+            txtLogin.DataBindings.Add("Text", tmp, "ID");
+            txtPassword.DataBindings.Add("Text", tmp, "Pass");
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT Count(*) FROM DangNhap WHERE ID=@id AND PassWord=@pw";
+            string a = txtLogin.Text.Trim();
+            string b = txtPassword.Text;
+            int x = new PhongBUS().Log(sql, a, b);
+            if (x == 1)
+            {
+                frmMain frmM = new frmMain();
+                frmM.Show();
+                if (cbSave.Checked)
+                {
+                    int i = new PhongBUS().Save(a, b);
+                }
+                else
+                {
+                    int j = new PhongBUS().Xoa();
+                }
+            }
+            else
+            {
+                MessageBox.Show("NoOK");
+            }
+
+        }
+
+        private void btReset_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void frmLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
         }
     }
 }
