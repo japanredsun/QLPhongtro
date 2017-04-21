b93e2a9e9953ae7964f518ac99442c9bd5e3567b
japanredsun <japanredsun@gmail.com>
2017-04-22 01:01:55 +0700

    Building new UI

--- MotelRoomManagement/MotelRoomManagement/Program.cs
+++ MotelRoomManagement/MotelRoomManagement/Program.cs
@@ -15,7 +15,7 @@ namespace MotelRoomManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new FrmGeneral());
         }
     }
 }
