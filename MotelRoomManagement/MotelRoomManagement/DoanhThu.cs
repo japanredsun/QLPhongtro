4a90c6a603f84a3290685eb32c3bc2d06420acd5
KDCuong <toilacaoboivientay96@gmail.com>
2017-05-23 14:02:23 +0700

    UPDoanhThu

---
+++ MotelRoomManagement/MotelRoomManagement/DoanhThu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MotelRoomManagement
+{
+    public partial class DoanhThu : UserControl
+    {
+        public DoanhThu()
+        {
+            InitializeComponent();
+        }
+
+        private void DoanhThu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
